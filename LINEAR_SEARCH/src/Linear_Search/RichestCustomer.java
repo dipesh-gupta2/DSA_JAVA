5de84f76a0c41c52b1ab821dce92c81cd0150127
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-07-25 11:09:40 +0530

    linear search program added

--- LINEAR_SEARCH/src/Linear_Search/RichestCustomer.java
+++ LINEAR_SEARCH/src/Linear_Search/RichestCustomer.java
@@ -1,5 +1,26 @@
 package Linear_Search;
 //https://leetcode.com/problems/richest-customer-wealth/
 public class RichestCustomer {
-    
+    public static void main(String[] args) {
+        int[][] accounts= {{1,2,3},{3,2,9}};
+        int maxWealth=maximumWealth(accounts);
+        System.out.println(maxWealth);
+    }
+    static int maximumWealth(int[][] accounts) {
+       int max=0;
+       for(int i=0;i<accounts.length;i++){
+           if(max<wealth(accounts[i])){
+               max=wealth(accounts[i]);
+           }
+       };
+       return max;
+    };
+    static int wealth(int[] account){
+        int sum=0;
+      for (int i=0;i<account.length;i++){
+          sum+=account[i];
+      }
+      return sum;
+    };
+
 }
